daab210d5519ab138d0439a0dc820392736a1f47
Lav Pathak <lav.pathak@gmail.com>
2024-02-08 13:29:44 -0700

    Making a test change.

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            FIB Calculator via K8s in GKE
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
